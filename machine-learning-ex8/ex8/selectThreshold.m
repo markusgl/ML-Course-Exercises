2dfd8d2e8da2c809ba25f622ad41bfe06aafe01a
markusgl <markus.glas@gmx.net>
2020-10-26 12:45:28 +0100

    task2 done

--- machine-learning-ex8/ex8/selectThreshold.m
+++ machine-learning-ex8/ex8/selectThreshold.m
@@ -24,17 +24,17 @@ for epsilon = min(pval):stepsize:max(pval)
     %       of 0's and 1's of the outlier predictions
 
  
-    for i = 1:length(pval)
-      cvPredictions = (pval(i) < epsilon);
-      tp = sum((cvPredictions(i) == 1) & (yval(i)==1));
-      fp = sum((cvPredictions(i) == 1) & (yval(i)==0));
-      fn = sum((cvPredictions(i) == 0) & (yval(i)==1));
 
-      prec = tp / (tp + fp);
-      rec = tp / (tp + fn);
+    cvPredictions = (pval < epsilon);
+    tp = sum((cvPredictions == 1) & (yval==1));
+    fp = sum((cvPredictions == 1) & (yval==0));
+    fn = sum((cvPredictions == 0) & (yval==1));
 
-      F1 = (2 * prec * rec) / (prec + rec);
-    endfor
+    prec = tp / (tp + fp);
+    rec = tp / (tp + fn);
+
+    F1 = (2 * prec * rec) / (prec + rec);
+
 
 
 
